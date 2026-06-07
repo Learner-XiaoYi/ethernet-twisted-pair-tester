6d1fdd2fafa5b0965748fce9f46a7ad6609d6ed3
xiaoyi <2965695492@qq.com>
2026-06-07 15:37:27 +0800

    更新GPIO驱动

--- TwistedPairTester/BSP/bsp_gpio.c
+++ TwistedPairTester/BSP/bsp_gpio.c
@@ -12,11 +12,20 @@ void BSP_GPIO_Set(BSP_GPIO_t *gpio)
                       GPIO_PIN_SET);
 }
 
-void BSP_GPIO_Reset(BSP_GPIO_t *gpio)
+uint8_t BSP_GPIO_Reset(BSP_GPIO_t *gpio)
 {
     HAL_GPIO_WritePin(gpio->port,
                       gpio->pin,
                       GPIO_PIN_RESET);
+}
+
+void BSP_GPIO_Write(BSP_GPIO_t *gpio, gpio_level_t level)
+{
+    HAL_GPIO_WritePin(
+        gpio->port,
+        gpio->pin,
+        (level == GPIO_HIGH) ? GPIO_PIN_SET : GPIO_PIN_RESET
+    );
 }
 
 void BSP_GPIO_Toggle(BSP_GPIO_t *gpio)
--- TwistedPairTester/BSP/bsp_gpio.h
+++ TwistedPairTester/BSP/bsp_gpio.h
@@ -16,9 +16,17 @@ typedef enum
     BSP_GPIO_HIGH
 }BSP_GPIO_Level_t;
 
+typedef enum
+{
+    GPIO_LOW = 0,
+    GPIO_HIGH = 1
+} gpio_level_t;
+
+
 void BSP_GPIO_Init(void);
 void BSP_GPIO_Set(BSP_GPIO_t *gpio);
-void BSP_GPIO_Reset(BSP_GPIO_t *gpio);
+uint8_t BSP_GPIO_Reset(BSP_GPIO_t *gpio);
+void BSP_GPIO_Write(BSP_GPIO_t *gpio, gpio_level_t level);
 void BSP_GPIO_Toggle(BSP_GPIO_t *gpio);
 BSP_GPIO_Level_t  BSP_GPIO_Read(BSP_GPIO_t *gpio);
 
