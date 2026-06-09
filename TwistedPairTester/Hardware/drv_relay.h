f549a198b60c169e0cfa3933ae4dc256006388fe
xiaoyi <2965695492@qq.com>
2026-06-09 10:40:47 +0800

    修复无声明问题

--- TwistedPairTester/Hardware/drv_relay.h
+++ TwistedPairTester/Hardware/drv_relay.h
@@ -3,6 +3,17 @@
 
 #include "bsp_gpio.h"
 
+void HW_Relay_Init(void);
+
+//同一时间只有一个吸合
+void HW_Relay_Select(uint8_t ch);
+
+//打开
+void HW_Relay_On(uint8_t ch);
+//关闭
+void HW_Relay_Off(uint8_t ch);
+//全部关闭
+void HW_Relay_AllOff(void);
 
 
 #endif
