8c7dbfd3ff2038a1d32cd1cc348467107afa7d1f
xiaoyi <2965695492@qq.com>
2026-06-09 13:14:18 +0800

    修复key隐患

--- TwistedPairTester/Hardware/drv_key.c
+++ TwistedPairTester/Hardware/drv_key.c
@@ -20,31 +20,31 @@ uint8_t KEY_Read(uint8_t id)
 volatile uint8_t key_1ms_flag = 0; //1ms定时标志
 void KEY_Scan(void)
 {
-		//每次扫描遍历按键
-    for(uint8_t i=0;i<2;i++)  
+    for(uint8_t i = 0; i < 2; i++)
     {
-        if(KEY_Read(i) == 0) //读取按键 i 的当前电平低电平有效
+        if(KEY_Read(i) == BSP_GPIO_LOW)   // 按下
         {
-            key_cnt[i]++;
+            if(key_cnt[i] < 1000)
+                key_cnt[i]++;
 
-            // 短按 连续按下 20ms，并且该按键的状态为 0
+            // 短按
             if(key_cnt[i] == 20 && key_state[i] == 0)
             {
-                key_event = (i==0)?KEY1_CLICK:KEY2_CLICK;
-                key_state[i] = 1;  //将状态置 1，该按键的短按事件已经产生
+                key_event = (i == 0) ? KEY1_CLICK : KEY2_CLICK;
+                key_state[i] = 1;
             }
 
-            // 长按 判断是否达到长按条件
+            // 长按
             if(key_cnt[i] == 1000 && key_state[i] < 2)
             {
-                key_event = (i==0)?KEY1_LONG:KEY2_LONG;
+                key_event = (i == 0) ? KEY1_LONG : KEY2_LONG;
                 key_state[i] = 2;
             }
         }
-        else
+        else  // 松开
         {
-            key_cnt[i] = 0;    //按键计数清0
-            key_state[i] = 0;  //按键状态清0
+            key_cnt[i] = 0;
+            key_state[i] = 0;
         }
     }
 }
