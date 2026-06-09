f88794ec03150a060c70d1bab19c1429702665ed
xiaoyi <2965695492@qq.com>
2026-06-09 09:54:04 +0800

    添加继电器驱动

---
+++ TwistedPairTester/Hardware/drv_relay.c
@@ -0,0 +1,75 @@
+#include "drv_relay.h"
+
+static BSP_GPIO_t relay_pin[8] =
+{
+    {GPIOF, GPIO_PIN_0},
+    {GPIOF, GPIO_PIN_1},
+    {GPIOF, GPIO_PIN_2},
+    {GPIOF, GPIO_PIN_3},
+    {GPIOF, GPIO_PIN_4},
+    {GPIOF, GPIO_PIN_5},
+    {GPIOF, GPIO_PIN_6},
+    {GPIOF, GPIO_PIN_7},
+};
+
+//默认全部关闭
+void HW_Relay_Init(void)
+{
+    uint8_t i;
+
+    for(i = 0; i < 8; i++)
+    {
+        BSP_GPIO_Mode(relay_pin[i], BSP_GPIO_MODE_OUTPUT_PP);
+
+        BSP_GPIO_Write(&relay_pin[i], BSP_GPIO_LOW);
+    }
+}
+
+//同一时间只有一个吸合
+void HW_Relay_Select(uint8_t ch)
+{
+    uint8_t i;
+
+    for(i = 0; i < 8; i++)
+    {
+        BSP_GPIO_Write(&relay_pin[i], BSP_GPIO_LOW);
+    }
+
+    if(ch < 8)
+    {
+        BSP_GPIO_Write(&relay_pin[ch], BSP_GPIO_HIGH);
+    }
+}
+
+//打开
+void HW_Relay_On(uint8_t ch)
+{
+    if(ch >= 8)
+    {
+        return;
+    }
+
+    BSP_GPIO_Write(&relay_pin[ch], BSP_GPIO_HIGH);
+}
+
+//关闭
+void HW_Relay_Off(uint8_t ch)
+{
+    if(ch >= 8)
+    {
+        return;
+    }
+
+    BSP_GPIO_Write(&relay_pin[ch], BSP_GPIO_LOW);
+}
+
+//全部关闭
+void HW_Relay_AllOff(void)
+{
+    uint8_t i;
+
+    for(i = 0; i < 8; i++)
+    {
+        BSP_GPIO_Write(&relay_pin[i], BSP_GPIO_LOW);
+    }
+}
---
+++ TwistedPairTester/Hardware/drv_relay.h
@@ -0,0 +1,9 @@
+#ifndef __DRV_RELAY_H
+#define __DRV_RELAY_H
+
+#include "bsp_gpio.h"
+
+
+
+#endif
+
