a868591394042abe5dbf7a4ef40d684b8e6bae6d
xiaoyi <2965695492@qq.com>
2026-06-12 14:52:44 +0800

    添加状态机

--- TwistedPairTester/App/app_ui.c
+++ TwistedPairTester/App/app_ui.c
@@ -8,6 +8,30 @@ void app_ui(void)
         case APP_PAGE_CALIBRATION:
             /* 校准界面 */
             {
+                switch (g_app.test)
+                {
+                    case APP_TEST_IDLE: //准备界面
+                    {
+
+                        break;
+                    }
+
+                    case APP_TEST_RUNNING: //桥准中界面
+                    {
+
+                        break;
+                    }
+
+                    case APP_TEST_FINISH: //桥准结束界面
+                    {
+
+                        break;
+                    }
+                        
+                    
+                    default:
+                        break;
+                }
                 
             }
             break;
