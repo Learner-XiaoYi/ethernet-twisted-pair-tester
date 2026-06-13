260801227217bfe316e1d2f45f22355676dbbf3e
xiaoyi <2965695492@qq.com>
2026-06-13 12:19:08 +0800

    更新校准逻辑

--- TwistedPairTester/Service/srv_length.c
+++ TwistedPairTester/Service/srv_length.c
@@ -6,26 +6,27 @@ const float CnumTemp_UTP = 65.0;
 
 
 //频率转电容  Ctemp = (1 / (0.693147 * (Res1 + 2 * Res2) * FreqNum)) * 1000000000000;
-float Length_ConvertFreqToCap(void)
+//freq，校准频率
+float Length_ConvertFreqToCap(int freq);
 {
-	uint16_t FreqNum = 0;
+	int FreqNum = 0;
 	float Ctemp = 0;
 	FreqNum = HW_Length_GetCount();
-	Ctemp = (1 / (0.693147 * (Res1 + 2 * Res2) * FreqNum)) * 1000000000000;
+	Ctemp = 1 / (0.693147 * (Res1 + 2 * Res2) * (FreqNum - freq)) * 1000000000000;
 	return Ctemp;
 }
 
 //电容转长度  LongNum = (Ctemp - Cbegin) / result_float;
-float Length_ConvertCapToMeter(float cap_total, float cap_offset_utp, float cap_offset_sftp)
+float Length_ConvertCapToMeter(float cap_total)
 {
     float length = 0.0f;
     switch (cable)
     {
         case CABLE_UTP:
-            length = (cap_total - cap_offset_utp) / CnumTemp_UTP;
+            length = (cap_total) / CnumTemp_UTP;
             break;
         case CABLE_SFTP:
-            length = (cap_total - cap_offset_sftp) / CnumTemp_SFTP;
+            length = (cap_total) / CnumTemp_SFTP;
             break;
     }
     return length;
--- TwistedPairTester/Service/srv_length.h
+++ TwistedPairTester/Service/srv_length.h
@@ -4,11 +4,12 @@
 #include "hw_cable.h"
 #include "drv_cable_test.h"
 
+
 #define Res2 98928
 #define Res1 51750
 
-float Length_ConvertFreqToCap(void);
-float Length_ConvertCapToMeter(float cap_total, float cap_offset, float cap_per_meter);
+float Length_ConvertFreqToCap(int freq);
+float Length_ConvertCapToMeter(float cap_total);
 
 #endif
 
